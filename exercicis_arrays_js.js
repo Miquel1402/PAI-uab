5513089369c75276a66d4e132eaed9f1bf6e53b7
Miquel1402 <1713425@uab.cat>
2025-11-28 18:21:20 +0100

    update

---
+++ exercicis_arrays_js.js
@@ -0,0 +1,11 @@
+// 21.1.- Fes un programa que faci el següent:
+// ● Defineix una variable de tipus taula, buida.
+// ● Afegeixi el valor “Hola” a la posició 0.
+// ● Afegeixi el valor “Adeu” a la posició 1.
+// ● Escrigui a la consola el contingut de la taula.
+
+
+let taula = [];
+taula[0] = "Hola";
+taula[1] = "Adeu";
+console.log(taula);
